f8eb4f2c64313c49001c6cd8a2eb482adfab3127
Mi-sho <kamenovv.m@gmail.com>
2025-04-06 22:56:30 +0300

    folder rename

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 
 import Navigation from './components/navigation/Navigation'
 import Home from './components/home-page/Home'
-import Login from './components/login-page/Login'
+import Login from './components/login/Login'
 import Register from './components/register-page/Register'
-import About from './components/about-us-page/About'
+import About from './components/about-us/About'
 import Footer from './components/footer/Footer'
 
 function App() {
