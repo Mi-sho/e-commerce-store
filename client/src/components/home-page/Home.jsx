80202faf8ccd798e99787f0f5162e9e62100f896
Mi-sho <kamenovv.m@gmail.com>
2025-04-06 21:32:06 +0300

    Add home page with hero section

---
+++ client/src/components/home-page/Home.jsx
@@ -0,0 +1,15 @@
+import HeroSection from './hero-section/HeroSection'
+import styles from './Home.module.css'
+
+
+export default function Home() {
+    return(
+        <>
+        <HeroSection />
+        <div className={styles["content-home-wrapper"]}>
+
+        
+        </div>
+        </>
+    )
+}
