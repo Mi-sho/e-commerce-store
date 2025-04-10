cd7d46ac5875b497d1ad1fc452f5a22a0aaec8b2
Mi-sho <kamenovv.m@gmail.com>
2025-04-10 13:18:52 +0300

    css adjustments

--- client/src/components/home-page/latest-items/LatestItems.jsx
+++ client/src/components/home-page/latest-items/LatestItems.jsx
@@ -79,7 +79,7 @@ export default function LatestItems() {
         </span>
         <Link
           to="#"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-black bg-latest border  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-4xl text-sm px-5 py-2.5 text-center hover:scale-[1.08]"
         >
           Add to cart
         </Link>
